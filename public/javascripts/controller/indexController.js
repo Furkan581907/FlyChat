0fa370c37ad8b1e1f19276abe5bd3b40fd81c6d3
Bekir Furkan Zadegil <furkan581907@Bekir-MacBook-Pro.local>
2020-11-01 02:30:01 +0300

    dotenv installed

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Bekir Furkan ZADEGİL
+ * ***** Github : Furkan581907 ******
+ * ***** İnstagram : Furkann.z ******
+ * ***** Web : www.zadegil.com ******
+ * ***** mail : zadegil581907@gmail.com ******
+ * ***** Bismillahirrahmanirrahim ******
+ * Created Date : 2020.
+ *
+ */
+
+app.controller('indexController',['$scope',($scope)=>{
+    console.log('hi');
+}])
