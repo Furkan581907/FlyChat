5edb07b6b2081b2172b5b9fec31b4ceed575bcf3
Bekir Furkan Zadegil <furkan581907@Bekir-MacBook-Pro.local>
2020-11-01 01:53:03 +0300

    angular js installed

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Bekir Furkan ZADEGİL
+ * ***** Github : Furkan581907 ******
+ * ***** İnstagram : Furkann.z ******
+ * ***** Web : www.zadegil.com ******
+ * ***** mail : zadegil581907@gmail.com ******
+ * ***** Bismillahirrahmanirrahim ******
+ * Created Date : 2020.
+ *
+ */
+
+const app = angular.module('flychat',[]);
