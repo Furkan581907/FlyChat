f040bb9783005109007a3ef18d14c04ff36ff5e8
Bekir Furkan Zadegil <zadegil581907@gmail.com>
2020-11-01 20:06:31 +0300

    mongodb connection

---
+++ helpers/db.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Bekir Furkan ZADEGİL
+ * ***** Github : Furkan581907 ******
+ * ***** İnstagram : Furkann.z ******
+ * ***** Web : www.zadegil.com ******
+ * ***** mail : zadegil581907@gmail.com ******
+ * ***** Bismillahirrahmanirrahim ******
+ * Created Date : 2020.
+ *
+ */
+
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect(process.env.DB_STRING);
+
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB: Error', err);
+    });
+
+    mongoose.Promise = global.Promise;
+};
